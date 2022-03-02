6d96218b96066d24f336ee95bac4fb93ab89d73d
priyankathaker06 <74635125+priyankathaker06@users.noreply.github.com>
2022-03-02 17:31:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var priyanka=createSprite(x,y,width,height)
+if(condition){
+    executing statement ;
+}
+else{}
+functions: function call and function declaration 
+for(initialization; conditional; incrementing;){
+
+}
+game state 
+classes and objects 
+class Priyanka{
+    constructor 
+    display
+}
+physics engine: engine, world, body
+constraints- movment of one body effects movemment of another body 
+API Calls 
+Application Programming Interface 
+JSON (JavaScript Object Notation)
+databases- read and write data 
